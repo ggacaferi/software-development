16624464fa04058d6f2247d3f144dcbf4c795cc9
Ajan Abazi <ajani@Ajans-MacBook-Pro.local>
2025-05-20 19:27:01 +0300

    Refactoring test cases to reach 69% coverage

--- __tests__/head-to-head-movement.test.js
+++ __tests__/head-to-head-movement.test.js
@@ -47,4 +47,35 @@ describe('headToHeadMovement', () => {
     headToHeadMovement(gameState, isMoveSafe);
     expect(isMoveSafe.up).toBe(true);
   });
+
+  it('should avoid head-to-head with multiple larger snakes', () => {
+    const gameState = {
+      you: {
+        id: 'me',
+        body: [{ x: 5, y: 5 }],
+        length: 2
+      },
+      board: {
+        snakes: [
+          {
+            id: 'enemy1',
+            body: [{ x: 5, y: 6 }], // enemy1 above
+            length: 3
+          },
+          {
+            id: 'enemy2',
+            body: [{ x: 4, y: 5 }], // enemy2 to the left
+            length: 4
+          }
+        ]
+      }
+    };
+
+    const isMoveSafe = { up: true, down: true, left: true, right: true };
+    headToHeadMovement(gameState, isMoveSafe);
+    expect(isMoveSafe.up).toBe(false);
+    expect(isMoveSafe.left).toBe(false);
+    expect(isMoveSafe.down).toBe(true);
+    expect(isMoveSafe.right).toBe(true);
+  });
 });
